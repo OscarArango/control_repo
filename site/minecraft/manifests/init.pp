4d7f8180e83322bfe8921318b927202559450446
Oscar Arango <OscarArango@users.noreply.github.com>
2021-04-30 17:46:40 -0400

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -19,7 +19,9 @@ ass minecraft (
   }
   file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
+    content => epp('minecraft/minecraft.service.epp', {
+      install_dir => $install_dir,
+    }),
   }
   service { 'minecraft':
     ensure => running,
