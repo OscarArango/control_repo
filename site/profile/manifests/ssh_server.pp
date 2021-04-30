6be75f82a3473bdd8e5f38eb58a3e360af35154b
Oscar Arango <OscarArango@users.noreply.github.com>
2021-04-30 13:30:43 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDXFWICwhl4K62pxED38q33XJlzIWsG/TYQskLyIfzMxDpmsLiDozeekZ3Qkuydt6KB7qJ/zk1oBbOAYnfHDD7WkGmoqzRUT01ZwGw9fVBxkWkIFCyFdbN8nDivrcaBbJPmersbBubE736o83+F/Io7+A5PTmvnmr1tcUtAdvIPAFFH6J5Eei7dpuhQ9LWXXhs4v4bkSXFyjYhRDM5NfK6QFI0jMCTlyoLPukrGotI4lA+qMrL7yJeY5DtxeJspqCsmZVSOYjDRJJCKwdFH8hXNO0If1x7L6dKpZl8nsNXgUsBJllzvrH9dW/OpJCjLPzdDzvczgKi2easkSt9NL+N9',
   }  
 }
